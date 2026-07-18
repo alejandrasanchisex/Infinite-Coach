42643f1420ebe92b2fa159b35dd2e1d7f41d8674
ingeniaingenia <ingenia@ia.com>
2026-07-18 23:46:00 +0200

    Bump version to v680 and fix monthlyFee display in client list

--- public/js/utils.js
+++ public/js/utils.js
@@ -1099,7 +1099,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (isTrainerPage && !isLoginPage) {
         if (typeof PersonalizationManager === 'undefined') {
             const script = document.createElement('script');
-            script.src = 'js/personalization.js?v=679';
+            script.src = 'js/personalization.js?v=680';
             script.onload = () => {
                 if (window.PersonalizationManager) {
                     window.PersonalizationManager.init();
