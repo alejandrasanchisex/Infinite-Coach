25bf0131964d8e0171fc50b05752851481a8e0e7
ingeniaingenia <ingenia@ia.com>
2026-07-24 10:46:29 +0200

    bump: release version 789

--- public/js/utils.js
+++ public/js/utils.js
@@ -1099,7 +1099,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (isTrainerPage && !isLoginPage) {
         if (typeof PersonalizationManager === 'undefined') {
             const script = document.createElement('script');
-            script.src = 'js/personalization.js?v=788';
+            script.src = 'js/personalization.js?v=789';
             script.onload = () => {
                 if (window.PersonalizationManager) {
                     window.PersonalizationManager.init();
